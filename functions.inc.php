6f4017d8ede3e9c09391e523f98fdf7d243d7e90
Petrut Darius-Ioan <131020605+petrut-darius@users.noreply.github.com>
2025-09-19 08:22:16 +0300

    update action

--- functions.inc.php
+++ functions.inc.php
@@ -1,15 +1,13 @@
-<?php//swithch to get for params
+<?php
 function gateway(string $request_method) {
-    $raw_data = file_get_contents("php://input");
-    $data = json_decode(($raw_data), true);
 
     
     $db = db_connection();
-
+    
     switch($request_method){
         case "GET":
-            if(isset($data["id"]) && !empty($data["id"])) {
-                $id = $data["id"];
+            if(isset($_GET["id"]) && $_GET["id"]) {
+                $id = $_GET["id"];
                 if($data = get_data($db, $id)){
                     if($data[1] >= 2) {
                         return json_encode(["msg" => "query error."], JSON_PRETTY_PRINT);
@@ -17,6 +15,8 @@ function gateway(string $request_method) {
                     }elseif($data[1] == 0) {
                         return json_encode(["msg" => "there is no game with the id: " . $id], JSON_PRETTY_PRINT);
                         //break;
+                    }elseif($data[1] == 1) {
+                        return json_encode($data, JSON_PRETTY_PRINT);
                     }
                 }else{
                     return json_encode(["msg" => "'get_data' fn error."], JSON_PRETTY_PRINT);
@@ -26,10 +26,11 @@ function gateway(string $request_method) {
                 return json_encode(get_all_data($db), JSON_PRETTY_PRINT);
                 //break;
             }
+            break;
         case "POST":
-            if(isset($data["name"]) && !empty($data["name"]) && isset($data["in_stock"]) && !empty($data["in_stock"])) {
-                $name = $data["name"];
-                $stock = ($data["in_stock"] === "yes") ? 1 : 0;
+            if(isset($_GET["name"]) && !empty($_GET["name"]) && isset($_GET["in_stock"]) && !empty($_GET["in_stock"])) {
+                $name = $_GET["name"];
+                $stock = ($_GET["in_stock"] === "yes") ? 1 : 0;
                 if(add_game($db, $name, $stock)) {
                     return json_encode(["msg" => "you added a game to the db."], JSON_PRETTY_PRINT);
                 }
@@ -37,9 +38,42 @@ function gateway(string $request_method) {
                 return json_encode(["msg" => "problem with the params you inserted."], JSON_PRETTY_PRINT);
             }
             break;
+        case "PATCH":
+        case "PUT":
+            if(isset($_GET["id"]) && !empty($_GET["id"]) && ((isset($_GET["name"]) && !empty($_GET["name"])) || (isset($_GET["in_stock"]) && !empty($_GET["in_stock"])))) {
+                $id = $_GET["id"];
+                $name = $_GET["name"] ?? null;
+                $stock = $_GET["in_stock"] ?? null;
+                $types = "";
+                if($name != null) {
+                    $fields[] = "name = ?";
+                    $params[] = $name;
+                    $types .= "s";
+                }
+
+                if($stock != null) {
+                    $fields[] = "in_stock = ?";
+                    $params[] = $stock;
+                    $types .= "i";
+                }
+
+                if(empty($fields) || empty($params) || empty($types)) {
+                    return json_encode(["msg" => "problem with getting data."], JSON_PRETTY_PRINT);
+                }
+
+                if(!empty($fields) && !empty($params) && !empty($types)) {
+                    $result = update_data($db, $id, $params, $fields, $types);
+                    return json_encode(["msg" => "updated the game with id: " . $id], JSON_PRETTY_PRINT);
+                }
+
+            }else{
+                return json_encode(["msg" => "problem with the params you inserted."], JSON_PRETTY_PRINT);
+            }
+
+            break;
         case "DELETE":
-            if(isset($data["name"]) && !empty($data["name"])) {
-                $name = $data["name"];
+            if(isset($_GET["name"]) && !empty($_GET["name"])) {
+                $name = $_GET["name"];
                 if($result = delete_game($db,$name)) {
                     return json_encode(["msg" => "you deleted " . $name . "."], JSON_PRETTY_PRINT);
                 }else{
@@ -106,6 +140,16 @@ function add_game($db, $game_name, $game_in_stock) {
     $stmt->bind_param("si", $game_name, $game_in_stock);
     $result = $stmt->execute();
     $stmt->close();
+    return $result;
+}
+
+function update_data($db, int $id, array $params, array $fields, string $types) {
+    $query = "UPDATE `games` SET " . implode(",", $fields) . " WHERE id = " . $id;
+    $stmt = $db->prepare($query);
+    $stmt->bind_param($types, ...$params);
+    $result = $stmt->execute();
+    $stmt->close();
+
     return $result;
 }
 
